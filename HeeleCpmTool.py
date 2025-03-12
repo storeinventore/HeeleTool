271669c8e3da13702c77c69632c9460e3173b2b7
cloud <faessaythowel@gmail.com>
2025-03-12 15:14:49 +0100

    HeeleCpmTool.py

--- HeeleCpmTool.py
+++ HeeleCpmTool.py
@@ -48,15 +48,15 @@ def banner(console):
     brand_name += "                ██╔══██║██╔══╝░░██╔══╝░░██║░░░░░██╔══╝░░\n" 
     brand_name += "                ██║░░██║███████╗███████╗███████╗███████╗\n" 
     brand_name += "                ╚═╝░░╚═╝╚══════╝╚══════╝╚══════╝╚══════╝\n" 
-colors = [
+    colors = [
         "rgb(75,0,130)", "rgb(102,0,153)", "rgb(123,31,162)", "rgb(147,112,219)", "rgb(186,85,211)",  
         "rgb(216,191,216)", "rgb(221,160,221)", "rgb(238,130,238)", "rgb(255,0,255)", "rgb(153,50,204)",  
         "rgb(139,0,139)"
 ]
 colorful_text = gradient_text(brand_name, colors)
 console.print(colorful_text)
-print(Colorate.Horizontal(Colors.purple_to_blue, '======================================================'))
-print(Colorate.Horizontal(Colors.purple_to_blue, '\t ПЕРЕД ИСПОЛЬЗОВАНИЕМ ЭТОГО ИНСТРУМЕНТА ВЫЙДИТЕ ИЗ CPM'))
+print(Colorate.Horizontal(Colors.red_to_purple, '======================================================'))
+print(Colorate.Horizontal(Colors.red_to_purple, '\t ПЕРЕД ИСПОЛЬЗОВАНИЕМ ЭТОГО ИНСТРУМЕНТА ВЫЙДИТЕ ИЗ CPM'))
 print(Colorate.Horizontal(Colors.red_to_purple, ' ОБМЕН КЛЮЧОМ ДОСТУПА НЕ РАЗРЕШЕН И БУДЕТ ЗАБЛОКИРОВАН'))
 print(Colorate.Horizontal(Colors.red_to_purple, f' Telegram: @{__CHANNEL_USERNAME__} Или @{__GROUP_USERNAME__}'))
 print(Colorate.Horizontal(Colors.red_to_purple, '======================================================'))
