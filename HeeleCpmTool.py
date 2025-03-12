8f7b0af87f286c2f0690f852c4526f1537668d56
cloud <faessaythowel@gmail.com>
2025-03-12 19:46:39 +0100

    HeeleCpmTool.py

--- HeeleCpmTool.py
+++ HeeleCpmTool.py
@@ -143,7 +143,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = storeinventore(acc_access_key)
+        cpm = StoreInventore(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
