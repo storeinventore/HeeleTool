30e3a6a29ef0ec6ff83ea90e8186fb0a20abefa9
cloud <faessaythowel@gmail.com>
2025-03-12 14:17:23 +0100

    HeeleCpmTool.py

--- HeeleCpmTool.py
+++ HeeleCpmTool.py
@@ -55,11 +55,12 @@ def banner(console):
 
     colorful_text = gradient_text(brand_name, colors)
     console.print(colorful_text)
-print(Colorate.Horizontal(Colors.purple_to_blue, '=================================================================='))
-print(Colorate.Horizontal(Colors.purple_to_blue, '\t PLEASE LOGOUT FROM CPM BEFORE USING THIS TOOL'))
-print(Colorate.Horizontal(Colors.purple_to_blue, ' SHARING THE ACCESS KEY IS NOT ALLOWED AND WILL BE BLOCKED'))
-print(Colorate.Horizontal(Colors.purple_to_blue, f' Telegram: @{__CHANNEL_USERNAME__} Or @{__GROUP_USERNAME__}'))
-print(Colorate.Horizontal(Colors.purple_to_blue, '=================================================================='))
+console.print(Colorate.Horizontal(Colors.purple_to_blue, '======================================================'))
+console.print(Colorate.Horizontal(Colors.purple_to_blue, '\t PLEASE LOGOUT FROM CPM BEFORE USING THIS TOOL'))
+console.print(Colorate.Horizontal(Colors.purple_to_blue, ' SHARING THE ACCESS KEY IS NOT ALLOWED AND WILL BE BLOCKED'))
+console.print(Colorate.Horizontal(Colors.purple_to_blue, f' Telegram: @{__CHANNEL_USERNAME__} Or @{__GROUP_USERNAME__}'))
+console.print(Colorate.Horizontal(Colors.purple_to_blue, '======================================================'))
+
 
 
 
