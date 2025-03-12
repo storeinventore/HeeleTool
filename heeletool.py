ab6eec35577e21436df1ae56b452c73aeb536214
cloud <faessaythowel@gmail.com>
2025-03-12 19:45:48 +0100

    heeletool.py

--- heeletool.py
+++ heeletool.py
@@ -2,7 +2,7 @@ import requests
 
 __ENDPOINT_URL__: str = "https://heelee.squareweb.app/api"
 
-class storeinventore:
+class StoreInventore:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
